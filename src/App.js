fd35d3a35c7c3cc10ade61eb453af7a670b1a4b4
Ashab-Asir <ashabasir101@gmail.com>
2024-10-31 10:55:48 +0600

    load all the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./style/app.scss";
 function App() {
+  const [surahs, setSurahs] = useState([]);
+  const [currentSurah, setCurrentSurah] = useState(null);
+  useEffect(() => {
+    // Fetch data on component mount
+    fetch("https://api.alquran.cloud/v1/quran/ar.alafasy")
+      .then((response) => response.json())
+      .then((data) => setSurahs(data.data.surahs))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  useEffect(() => {
+    if (surahs.length > 0) {
+      setCurrentSurah(surahs[0]);
+    }
+  }, [surahs]);
+
   return (
     <div className="App">
-      <Song></Song>
+      <Song currentSurah={currentSurah}></Song>
       <Player></Player>
     </div>
   );
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,14 @@
 import React from "react";
-
-export default function Song() {
+import quranImage from "../assets/Sworn.jpg";
+export default function Song({ currentSurah }) {
+  if (!currentSurah) {
+    return <div>Loading...</div>; // Show a loading message or placeholder
+  }
+  console.log(currentSurah);
   return (
     <div className="song-container">
-      <h1>Picture</h1>
-      <h1>name</h1>
-      <h1>artist</h1>
+      <img src={quranImage} alt="qruan" />
+      <h2>{currentSurah.englishName}</h2>
     </div>
   );
 }
